792cf7960a5f5efa6e0f9d856107714b63833067
MTG\mtg09 <mtg.software.dev@gmail.com>
2022-12-14 12:22:44 +0400

    update (search): add debouncing

--- src/Components/Navbar/Search.tsx
+++ src/Components/Navbar/Search.tsx
@@ -1,20 +1,20 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Hits, SearchBox } from "react-instantsearch-hooks-web";
 import { Projects } from "src/graphql";
 import { openModal } from "react-url-modal";
+import { useDebouncedCallback } from "@react-hookz/web";
 
 export default function Search() {
   const [showSearchResults, setShowSearchResults] = useState(false);
 
-  const autoFocus = useCallback((node: HTMLElement | null) => {
-    if (node) {
-      console.log(node.querySelector("input"));
-      node.querySelector("input")?.focus();
-    }
-  }, []);
+  const debouncedSearch = useDebouncedCallback(
+    (query: string, search: (v: string) => void) => search(query),
+    [],
+    300
+  );
 
   return (
-    <div className="relative grow max-w-[420px]" ref={autoFocus}>
+    <div className="relative grow max-w-[420px]">
       <SearchBox
         autoFocus
         placeholder="Search projects"
@@ -27,7 +27,8 @@ export default function Search() {
             "absolute left-16 top-1/2 -translate-y-1/2 -translate-x-1/2 text-body2",
         }}
         onFocus={() => setShowSearchResults(true)}
-        onBlur={() => setTimeout(() => setShowSearchResults(false), 50)}
+        onBlur={() => setTimeout(() => setShowSearchResults(false), 100)}
+        queryHook={debouncedSearch}
       />
       {showSearchResults && (
         <div className="absolute top-[calc(100%+16px)] w-[max(100%,360px)] bg-white border border-gray-200 p-16 rounded shadow-lg max-h-[400px] overflow-y-scroll">
@@ -68,14 +69,6 @@ function Hit({ hit }: { hit: SearchProject }) {
       className="hover:bg-gray-50 p-12 text-left w-full flex gap-12 items-center"
       onClick={clickProject}
     >
-      <img
-        src={
-          hit.logo ??
-          `https://avatars.dicebear.com/api/initials/${hit.title}.svg`
-        }
-        alt=""
-        className="w-48 aspect-square rounded-full object-cover bg-white border border-gray-200"
-      />
       <div className="justify-around items-start min-w-0 flex-1 hover:cursor-pointer">
         <p className="text-body4 w-full font-bold overflow-ellipsis overflow-hidden whitespace-nowrap">
           {hit?.title}
